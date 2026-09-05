0d15f8367409e44f67ff8a1e368c74d53eb5b985
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-09-05 14:02:07 +0100

    modify lms-frontend folder

--- LMS-Application/lms-frontend/src/pages/Register.jsx
+++ LMS-Application/lms-frontend/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     setError("");
     setSuccess("");
     try {
-      await axios.post("http://127.0.0.1:8009/api/user/auth/", form);
+      await axios.post("http://16.170.230.162:8009/api/user/auth/", form);
       setSuccess("Registration successful! Redirecting to login...");
       setTimeout(() => navigate("/login"), 1500);
     } catch (err) {
